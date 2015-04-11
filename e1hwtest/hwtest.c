319927c315a551e9913692570ad1a0f56e9c9d3d
vladimir_ikpi32 <vladimir@(none)>
2015-04-11 20:14:28 +0300

    EXTI not working, otherwise it is more or less ok

--- e1hwtest/hwtest.c
+++ e1hwtest/hwtest.c
@@ -24,28 +24,24 @@
 void __aeabi_idiv(void){
 }
 
-volatile uint32_t sensors[4]={0,0,0,0};
-volatile uint32_t sensors_old[4]={0,0,0,0};
+volatile uint32_t sensors[4]={1,2,3,4};
+volatile uint32_t sensors_old[4]={2,3,4,5};
 volatile uint32_t time=0;
 
 char bits[4]={14,15,8,9};
 
 void print_int(uint32_t integer_n){
-    char buffer[11];
+    char buffer[12]=" 0000000000";
     int i;
-    for(i=0;i<sizeof(buffer);i++){
-	buffer[i]='0';
-    };
-    buffer[10]=0;
-    i=9;
+    i=10;
     while(integer_n>0){
 	int modulus;
 	modulus=integer_n % 10;
-	buffer[i]=char('0'+modulus);
+	buffer[i]=(char)('0'+modulus);
 	i--;
 	integer_n /=10;
     }
-    puts(buffer);
+    for(i=0;buffer[i]!=0;i++) putchar(buffer[i]);
 }
 
 void systick_handler( void){
@@ -54,20 +50,20 @@ void systick_handler( void){
 
 void exti_handler( void ){
     int i;
+    if(b5_get(ONBOARD_LED)) b5_clear(ONBOARD_LED); else b5_set(ONBOARD_LED);
     for(i=0;i<4;i++){
 	    if(EXTI_PR & (1<<bits[i])){
 		sensors_old[i]=sensors[i];
 		sensors[i]=time;
-		EXTI_PR |= (1<<bits[i]);
 	    }
     }
+    EXTI_PR = 0xffff;
 }
 
 void exti_init( void) {
     EXTI_IMR = 0xc300;
     EXTI_EMR = 0xc300;
     EXTI_RTSR = 0xc300;
-    EXTI_FTSR = 0xc300;
     AFIO_EXTICR3 = 0x0022;
     AFIO_EXTICR4 = 0x3300;
 }
@@ -79,29 +75,10 @@ void hardware_setup( void )
   //For running at proper speed
   rcc_clock_setup_in_hse_25mhz_out_72mhz(  );
   gpio_setup(  );
-  exti_init( );
   
-#ifdef _LINK_RAM_
+//#ifdef _LINK_RAM_
   nvic_init( );
-#endif
-  nvic_register_interrupt(NVIC_SYSTICK_IRQ,systick_handler);
-  systick_set_reload(9000);
-  systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8);
-  systick_counter_enable();
-  systick_interrupt_enable();
-  nvic_enable_irq(NVIC_SYSTICK_IRQ);
-
-  nvic_register_interrupt(NVIC_EXTI0_IRQ,exti_handler);
-  nvic_register_interrupt(NVIC_EXTI1_IRQ,exti_handler);
-  nvic_register_interrupt(NVIC_EXTI2_IRQ,exti_handler);
-  nvic_register_interrupt(NVIC_EXTI3_IRQ,exti_handler);
-  nvic_register_interrupt(NVIC_EXTI4_IRQ,exti_handler);
-  nvic_enable_irq(NVIC_EXTI0_IRQ);
-  nvic_enable_irq(NVIC_EXTI1_IRQ);
-  nvic_enable_irq(NVIC_EXTI2_IRQ);
-  nvic_enable_irq(NVIC_EXTI3_IRQ);
-  nvic_enable_irq(NVIC_EXTI4_IRQ);
-
+//#endif
 
   //init USB  
   Set_USBClock(  );
@@ -118,17 +95,25 @@ void hardware_setup( void )
   //I2C init
   TwInit( 0 );
   
-/*
-  exti_set_trigger(EXTI8, EXTI_TRIGGER_RISING);
-  exti_set_trigger(EXTI9, EXTI_TRIGGER_RISING);
-  exti_set_trigger(EXTI14, EXTI_TRIGGER_RISING);
-  exti_set_trigger(EXTI15, EXTI_TRIGGER_RISING);
-  exti_select_source(EXTI8, GPIOC);
-  exti_select_source(EXTI9, GPIOC);
-  exti_select_source(EXTI14, GPIOD);
-  exti_select_source(EXTI15, GPIOD);
-  exit_enable_request(EXTI8|EXTI9|EXTI14|EXTI15);
-*/
+  nvic_register_interrupt(NVIC_SYSTICK_IRQ,systick_handler);
+  systick_set_reload(9000);
+  systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8);
+  systick_counter_enable();
+  systick_interrupt_enable();
+  nvic_enable_irq(NVIC_SYSTICK_IRQ);
+
+  exti_init( );
+  nvic_register_interrupt(NVIC_EXTI0_IRQ,exti_handler);
+  nvic_register_interrupt(NVIC_EXTI1_IRQ,exti_handler);
+  nvic_register_interrupt(NVIC_EXTI2_IRQ,exti_handler);
+  nvic_register_interrupt(NVIC_EXTI3_IRQ,exti_handler);
+  nvic_register_interrupt(NVIC_EXTI4_IRQ,exti_handler);
+  nvic_enable_irq(NVIC_EXTI0_IRQ);
+  nvic_enable_irq(NVIC_EXTI1_IRQ);
+  nvic_enable_irq(NVIC_EXTI2_IRQ);
+  nvic_enable_irq(NVIC_EXTI3_IRQ);
+  nvic_enable_irq(NVIC_EXTI4_IRQ);
+
 };
 
 unsigned char byte = 0x0;
@@ -142,16 +127,18 @@ int main( void )
 
   while( 1 ) {
     byte = getchar( );
-    for(i=0;i<3;i++){
-	print_int(sensors[0]);
-	puts(" ");
+    puts("time:\r\n");
+    print_int(time);
+    puts("new:\r\n");
+    for(i=0;i<4;i++){
+	print_int(sensors[i]);
     };
-    puts("\n");
-    for(i=0;i<3;i++){
-	print_int(sensors_old[0]);
-	puts(" ");
+    puts("");
+    puts("old:\r\n");
+    for(i=0;i<4;i++){
+	print_int(sensors_old[i]);
     };
-    puts("\n\n");
+    puts("\r\n");
   };
 
   return 0;
