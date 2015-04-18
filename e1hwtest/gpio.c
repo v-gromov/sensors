93f316d4b8c1bf6b4fd7bb376f2c921102b5836e
vladimir_ikpi32 <vladimir@(none)>
2015-04-18 20:05:35 +0300

    Реализован подсчёт оборотов

--- e1hwtest/gpio.c
+++ e1hwtest/gpio.c
@@ -18,6 +18,7 @@ void common_gpio_setup( void )
 {
   rcc_peripheral_enable_clock( &RCC_APB2ENR,
                                IOPEEN | IOPDEN | IOPBEN | IOPCEN | AFIOEN );
+  rcc_peripheral_enable_clock( &RCC_APB1ENR, TIM3EN|TIM4EN);
 
   //Sensors
   b5_set_mode( SENSOR1, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT);
@@ -26,7 +27,7 @@ void common_gpio_setup( void )
   b5_set_mode( SENSOR4, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT);
   //LED
   b5_set_mode( ONBOARD_LED, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL);
-
+  AFIO_MAPR |= AFIO_MAPR_TIM4_REMAP | AFIO_MAPR_TIM3_REMAP_FULL_REMAP;
 };
 
 /** \}
--- e1hwtest/hwtest.c
+++ e1hwtest/hwtest.c
@@ -2,6 +2,7 @@
 #include <libopenstm32/rcc.h>
 #include <libopenstm32/nvic.h>
 #include <libopenstm32/exti.h>
+#include <libopenstm32/timer.h>
 #include <libopenstm32/systick.h>
 #include <stm32_usb_init.h>
 #include <stm32_usb_pwr.h>
@@ -53,25 +54,49 @@ void systick_handler( void){
     time ++;
 }
 
-void exti_handler( void ){
-    int i;
-    uint32_t flags = EXTI_PR;
-    EXTI_PR = 0xffff;
-    if(b5_get(ONBOARD_LED)) b5_clear(ONBOARD_LED); else b5_set(ONBOARD_LED);
-    for(i=0;i<4;i++){
-	    if(flags & (1<<bits[i])){
-		sensors_old[i]=sensors[i];
-		sensors[i]=time;
-	    }
-    }
+void timer_handler(void){
+     uint32_t flags3,flags4;
+     flags3 = TIM3_SR;//Берём статус таймера 3 (два датчика)
+     flags4 = TIM4_SR;//Берём статус таймера 4 (два датчика)
+     TIM3_SR = 0;//Сброс статуса (чтобы срабатывали прерывания дальше)
+     TIM4_SR = 0;
+     if(flags3 & TIM_SR_CC3IF){//Сработал захват таймера
+	sensors_old[0]=sensors[0];
+        sensors[0]=TIM3_CCR3;//Считать захваченный момент
+     }
+     if(flags4 & TIM_SR_CC3IF){
+	sensors_old[2]=sensors[2];
+    	sensors[2]=TIM4_CCR3;
+     }
+     if(flags3 & TIM_SR_CC4IF){
+	sensors_old[1]=sensors[1];
+    	sensors[1]=TIM3_CCR4;
+     }
+     if(flags4 & TIM_SR_CC4IF){
+	sensors_old[3]=sensors[3];
+    	sensors[3]=TIM4_CCR4;
+     }
 }
 
-void exti_init( void) {
-    EXTI_IMR = 0xc300;
-    EXTI_EMR = 0xc300;
-    EXTI_RTSR = 0xc300;
-    AFIO_EXTICR3 = 0x0022;
-    AFIO_EXTICR4 = 0x3300;
+void timer_init(void){
+     TIM3_PSC = 36000;//Предделитель таймера - на входе - 36МГц, 36МГц/36000 = таймер тикает раз в 1 миллисекунду (слишком редко)
+     TIM4_PSC = 36000;
+     TIM3_CCMR2 = 0;//Сбросить настройки таймера
+     TIM4_CCMR2 = 0;
+     TIM3_CCMR2 |= TIM_CCMR2_CC4S_IN_TI1|TIM_CCMR2_CC3S_IN_TI1;//Захват с каналов T4/T3
+     TIM4_CCMR2 |= TIM_CCMR2_CC4S_IN_TI1|TIM_CCMR2_CC3S_IN_TI1;
+     TIM3_CCMR2 |= TIM_CCMR2_IC3F_CK_INT_N_8 | TIM_CCMR2_IC4F_CK_INT_N_8;//Фильтр на входе таймера на 8 тактов (какой частоты и чего?)
+     TIM4_CCMR2 |= TIM_CCMR2_IC3F_CK_INT_N_8 | TIM_CCMR2_IC4F_CK_INT_N_8;
+     TIM3_CCER |= TIM_CCER_CC3P | TIM_CCER_CC4P;//Выбор захвата по фронту или по спаду
+     TIM4_CCER |= TIM_CCER_CC3P | TIM_CCER_CC4P;
+     TIM3_CCMR2 |= TIM_CCMR2_IC3PSC_OFF | TIM_CCMR2_IC4PSC_OFF;//Выключить предделитель для захвата - если магнитов больше одного - этой настройкой можно компенсировать количество
+     TIM4_CCMR2 |= TIM_CCMR2_IC3PSC_OFF | TIM_CCMR2_IC4PSC_OFF;
+     TIM3_CCER |= TIM_CCER_CC3E | TIM_CCER_CC4E;//Разрешить захват
+     TIM4_CCER |= TIM_CCER_CC3E | TIM_CCER_CC4E;
+     TIM3_CR1 |= TIM_CR1_CEN;//Разрешить счёт таймеру
+     TIM4_CR1 |= TIM_CR1_CEN;
+     TIM3_DIER |= TIM_DIER_CC3IE|TIM_DIER_CC4IE;//Включить прерывания по захвату от таймера
+     TIM4_DIER |= TIM_DIER_CC3IE|TIM_DIER_CC4IE;
 }
 
 /** NOTE: this function is called before FPGA is loaded.
@@ -108,18 +133,11 @@ void hardware_setup( void )
   systick_interrupt_enable();
   nvic_enable_irq(NVIC_SYSTICK_IRQ);
 
-  nvic_register_interrupt(NVIC_EXTI0_IRQ,exti_handler);
-  nvic_register_interrupt(NVIC_EXTI1_IRQ,exti_handler);
-  nvic_register_interrupt(NVIC_EXTI2_IRQ,exti_handler);
-  nvic_register_interrupt(NVIC_EXTI3_IRQ,exti_handler);
-  nvic_register_interrupt(NVIC_EXTI4_IRQ,exti_handler);
-  exti_init( );
-  EXTI_PR = 0xffff;
-  nvic_enable_irq(NVIC_EXTI0_IRQ);
-  nvic_enable_irq(NVIC_EXTI1_IRQ);
-  nvic_enable_irq(NVIC_EXTI2_IRQ);
-  nvic_enable_irq(NVIC_EXTI3_IRQ);
-  nvic_enable_irq(NVIC_EXTI4_IRQ);
+  timer_init();
+  nvic_register_interrupt(NVIC_TIM3_IRQ, timer_handler);//Зарегистрировать прерывание от таймера 3
+  nvic_register_interrupt(NVIC_TIM4_IRQ, timer_handler);
+  nvic_enable_irq(NVIC_TIM3_IRQ);//Разрешить прерывание от таймера 3 
+  nvic_enable_irq(NVIC_TIM4_IRQ);
 
 };
 
@@ -143,6 +161,16 @@ int main( void )
     puts("old:\r\n");
     for(i=0;i<4;i++){
 	print_int(sensors_old[i]);
+    };
+    puts("RPM:\r\n");
+    for(i=0;i<4;i++){
+	int rpm=0;
+	rpm = sensors[i]-sensors_old[i]+65536;//Считаем разницу во времени.
+	rpm = rpm % 65536;//Компенсируем отрицательные значения, так как таймер 16-бит = 65536
+        if(rpm!=0){
+	   rpm = 30000/rpm;//Считаем обороты в минуту, так как таймер тикает 1 раз в 1 миллисекунду = то по формуле 60 секунд/2*период
+	}
+	print_int(rpm);
     };
     puts("\r\n");
     if(b5_get(ONBOARD_LED)) b5_clear(ONBOARD_LED); else b5_set(ONBOARD_LED);
